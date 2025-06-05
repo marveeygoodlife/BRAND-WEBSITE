96b436f75f926296c0fb88ce28453fe5c3324a5a
Marvellous Ediagbonya <marvellousediagbonya683@gmail.com>
2025-06-05 20:40:48 +0100

    @ script.js --  I added form input validation for the enrollment form. @style.css -- fixed the scroll to top btn for enrollment form. user inputs are successfully logged to the console. NExt: sebd the inputs to databasde

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const linksContainer = document.querySelector(".links-container");
 const nav = document.querySelector("#nav");
 const topScroll = document.querySelector("#top-link");
 const body = document.documentElement;
-
+let valid = true;
  /* set date */
 myDate.innerHTML = new Date().getFullYear();
 
@@ -19,7 +19,6 @@ const currentTheme = localStorage.getItem('theme');
    if (currentTheme) {
       body.classList.add(currentTheme);
    }
-
  
 window.addEventListener('DOMContentLoaded', () => { 
    
@@ -30,22 +29,22 @@ window.addEventListener('DOMContentLoaded', () => {
    if (body.classList.contains("dark-mode")) {
       localStorage.setItem("theme", "dark-mode")
    } else {
-            localStorage.setItem("theme", "");
+      localStorage.setItem("theme", "");
    }
  })
- /* scroll to top */
+ /* dynamic nav height */
 btn.addEventListener('click', ()=>{
     const linksHeight = ul.getBoundingClientRect().height;
     const containerHeight = linksContainer.getBoundingClientRect().height;
 
      if (containerHeight === 0) {
-        linksContainer.style.height =`${linksHeight}px`;
+linksContainer.style.height =`${linksHeight}px`;
      } else {
-         linksContainer.style.height = 0;
+   linksContainer.style.height = 0;
        }
 });
 })
-
+/* scroll to top */
 window.addEventListener("scroll", ()=>{
    const scrollHeight = window.pageYOffset;
   const navheight = nav.getBoundingClientRect().height;
@@ -54,10 +53,10 @@ window.addEventListener("scroll", ()=>{
   if (scrollHeight > 1000) {
      topScroll.classList.add("show-link")
    } else {
-        topScroll.classList.remove("show-link")
+    topScroll.classList.remove("show-link")
   }
 
- /* add fixed nav */
+ /* add fixed nav bar */
  if (scrollHeight > navheight) {
    nav.classList.add('fixed-navbar');
  } else {
@@ -66,3 +65,120 @@ window.addEventListener("scroll", ()=>{
 })
 
 
+/* enrollment form validation */
+const enrolForm = document.getElementById("myform");
+const submitBtn = document.querySelector("button");
+ 
+if (enrolForm) {
+document.getElementById("myform").addEventListener("submit", (e)=>{
+e.preventDefault();
+/* fieldset 1 (user details) */
+const firstname = document.querySelector("#firstname").value.trim(); const firstnameError = document.getElementById("firstnameError");
+
+const lastname = document.querySelector("#lastname").value.trim(); const lastnameError = document.getElementById("lastnameError");
+
+const email = document.querySelector("#email").value.trim(); const emailError = document.getElementById("emailError");
+
+const phonenumber = document.querySelector("#telephone").value.trim(); const telephoneError = document.getElementById("telephoneError");
+
+/* fieldset 2 (work experience) */
+const selectstudent = document.querySelector("#studentoption").value.trim();
+const companyname = document.querySelector("#companyname").value.trim();
+const role = document.querySelector("#role").value.trim();
+const jobyears = document.querySelector("#jobyears").value.trim();
+
+/* fieldset 3(schedule program) */
+const dateinput = document.querySelector("#date").value.trim();
+
+/* fieldset 4 (register program) */
+const registerprogram = document.querySelector("#registerprogram").value.trim(); const registerError = document.getElementById("registerError");
+
+const internshipduration = document.querySelector("#duration").value.trim(); const internError = document.getElementById("internError");
+
+const country = document.querySelector("#country").value.trim(); const countryError = document.getElementById("countryError");
+
+/* fieldset 5 (find us) */
+const knowus = document.querySelector("#connect").value.trim();
+
+const checkbox = document.querySelector("#terms").checked; const checkError = document.getElementById("checkError"); 
+
+/* clear box */
+firstnameError.textContent = "";
+lastnameError.textContent = "";
+emailError.textContent = "";
+telephoneError.textContent = "";
+registerError.textContent = "";
+internError.textContent = "";
+countryError.textContent = "";
+checkError.textContent = "";
+
+/* validate all inputs */
+if (!firstname) {
+firstnameError.textContent = "Enter firstname";
+  valid = false;
+  return;
+ }
+if (!lastname) {
+lastnameError.textContent ="Enter Lastname";
+   valid = false;
+   return;
+}
+if (!email) {
+emailError.textContent = "Enter Email";
+   valid = false;
+   return;
+}
+if (!phonenumber) {
+telephoneError.textContent = "Enter Telephone  Number";
+   valid = false;
+   return;
+}
+if (!selectstudent) {
+   console.log("select option")
+   valid = false;
+   return;
+}
+if (!registerprogram) {
+registerError.textContent = "choose program";
+   valid = false;
+   return;
+}
+if (!internshipduration) {
+internError.textContent = "Select duration";
+   valid = false;
+   return;
+}
+if (!country) {
+countryError.textContent = "Select Country";
+   valid = false;
+   return;
+}
+if (!checkbox) {
+checkError.textContent = "Select terms";
+   valid = false;
+   return;
+}
+
+const userData = {
+   firstname,
+   lastname,
+   email,
+   phonenumber,
+   selectstudent,
+   companyname,
+   role,
+   jobyears,
+   internshipduration,
+   dateinput,
+   registerprogram,
+   country,
+   knowus,
+   checkbox
+}
+console.log("userData", userData)
+if (valid) {
+   enrolForm.submit();
+}
+
+});
+} 
